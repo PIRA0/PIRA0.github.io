45209a9c6c81ab6d1e0a6a6a9595b5b84c44532c
PIRA0 <114571702+PIRA0@users.noreply.github.com>
2024-12-11 11:44:14 +0100

    Update pag.js

--- ana2.1/pag.js
+++ ana2.1/pag.js
@@ -1,21 +1,21 @@
 window.onload = function() {
-    var dati = localStorage.getItem('datiUtente');  // Recuperiamo la stringa salvata
+    var dati = localStorage.getItem('datiUtente');
     if (dati) {
-        var datiArray = dati.split(',');  // Separiamo la stringa in un array
+        var datiArray = dati.split(',');
         var tabella = document.getElementById('tabella_tutta'); 
         var tbody = tabella.getElementsByTagName('tbody')[0]; 
         
-        // Creiamo una nuova riga nella tabella
+
         var tr = document.createElement('tr');
         
-        // Usando un ciclo for per aggiungere i dati alle celle della riga
+
         for (var i = 0; i < datiArray.length; i++) {
             var td = document.createElement('td');
-            td.textContent = datiArray[i];  // Impostiamo il valore della cella
+            td.textContent = datiArray[i]; 
             tr.appendChild(td);
         }
 
-        // Aggiungiamo la riga al corpo della tabella
+
         tbody.appendChild(tr);
     } else {
         alert("Nessun dato trovato nel localStorage.");
