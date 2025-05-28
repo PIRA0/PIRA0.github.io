3edf42b27d970aa7684aaeb9756b33a008624490
PIRA0 <114571702+PIRA0@users.noreply.github.com>
2025-05-28 10:10:07 +0200

    Delete ecom directory

--- ecom/Account.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-
-    let user = document.getElementById('User');
-    user.textContent = localStorage.getItem('user') || 'N/D';
-
-    let password = document.getElementById('Password');
-    password.textContent = localStorage.getItem('password') || 'N/D';
-};
